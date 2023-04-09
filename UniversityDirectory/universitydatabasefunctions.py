e9d449c5739881a90d8d8f960efe9b7887dca119
Maor Krasner <maorkrasner02@gmail.com>
2023-04-09 11:42:01 +0300

    added functions to database

--- UniversityDirectory/universitydatabasefunctions.py
+++ UniversityDirectory/universitydatabasefunctions.py
@@ -2,12 +2,17 @@ import os
 import sqlite3
 import fileconvertor
 import hmacfunctions
+import sys
 
-conn = sqlite3.connect('UniversityDatabase.txt')
+conn = sqlite3.connect('UniversityDatabase.db')
 
 cursor = conn.cursor()
 
-university_directory_path = "C:\\Users\\maork\\OneDrive\\Desktop\\WeHireCyberProject\\UniversityStudentsDiplomasFolder\\"
+university_directory_path = "/Users/maorkrasner/Desktop/WeHireCyberProject/UniversityStudentsDiplomasFolder/"
+
+#def determine_university_directory_path():
+#    if sys.platform == 'darwin':
+
 
 
 def insert_row_into_table(table_name, values_tuple):
@@ -38,16 +43,25 @@ def find_student_in_students_table(id_of_student):
 
 def create_diploma_and_sign_it(id_of_student):
     if not find_student_in_students_table(id_of_student):
-        return False
+        return False, None
+
+    find_student_name_query = "SELECT First ||' '|| Last FROM students WHERE ID = " + str(id_of_student) + ";"
+    name_query_res = cursor.execute(find_student_name_query).fetchone()
+
+    path_to_check_if_dimploma_already_exists = "/Users/maorkrasner/Desktop/WeHireCyberProject/UniversityStudentsDiplomasFolder/" + str(name_query_res[0]) + " " + str(id_of_student) + ".pdf"
+
+    if os.path.exists(path_to_check_if_dimploma_already_exists):
+        with open(path_to_check_if_dimploma_already_exists, 'r', encoding="utf8") as f:
+            pdf_file_content = f.read()
+        f.close()
+
+        return True, pdf_file_content
 
     find_grades_for_courses_query = """SELECT subjects.Subject_name, grades.grade
                                     FROM subjects INNER JOIN (grades INNER JOIN students ON grades.Student_ID = students.ID) 
                                     ON subjects.Subject_ID = grades.Subject_ID 
                                     WHERE students.ID = """ + str(id_of_student) + ";"
     grades_for_courses_list = cursor.execute(find_grades_for_courses_query).fetchall()
-
-    find_student_name_query = "SELECT First ||' '|| Last FROM students WHERE ID = " + str(id_of_student) + ";"
-    name_query_res = cursor.execute(find_student_name_query).fetchone()
 
     grades_for_courses_list.insert(0, name_query_res)
 
@@ -71,14 +85,19 @@ def create_diploma_and_sign_it(id_of_student):
 
     with open(pdf_file_path, 'rb') as pdf_file_to_sign:
         message_to_sign = pdf_file_to_sign.read()
+    pdf_file_to_sign.close()
 
     pdf_file_sign_digest = hmacfunctions.hmac_sign_with_sha256(key, message_to_sign)
 
     print("file digest/tag = " + str(pdf_file_sign_digest.hex()))
 
-    insert_row_into_table("certificates", (id_of_student, str(pdf_file_path.split("\\")[-1]), str(pdf_file_sign_digest.hex()), key.decode()))
+    insert_row_into_table("certificates", (id_of_student, str(pdf_file_path.split("/")[-1]), str(pdf_file_sign_digest.hex()), key.decode()))
 
-    return True
+    with open(pdf_file_path, 'r', encoding="utf8") as file_to_send:
+        message_to_send = file_to_send.read()
+    file_to_send.close()
+
+    return True, message_to_send
 
 
 def verify_if_the_certificate_is_real(id_of_student, message_that_student_passed):
@@ -104,6 +123,9 @@ def close_connection():
 
 
 if __name__ == '__main__':
+    relative_path = "UniversityStudentsDiplomasFolder"
+    absolute_path = os.path.abspath(relative_path)
+    print("Absolute path of ", relative_path, " is : ", absolute_path)
      #cursor.execute("DROP TABLE IF EXISTS certificates")
      #table = """CREATE TABLE certificates
      #       (Student_ID VARCHAR(25) PRIMARY KEY NOT NULL,
@@ -122,28 +144,34 @@ if __name__ == '__main__':
     # insert_row_into_table("students", (213225580, "Michal", "Shammai"))
     # print_all_rows_in_table("students")
 
-    current_student_id = 213225576
+    #current_student_id = 213225577
 
-    #val = create_diploma_and_sign_it(current_student_id)
-    #print(val)
+    #cursor.execute("DELETE FROM certificates WHERE Student_ID = " + str(current_student_id))
+    #conn.commit()
 
-    signed_file_name_query = "SELECT Signed_file_name from certificates WHERE Student_ID = " + str(current_student_id) + ";"
-    signed_file_name_query_result = cursor.execute(signed_file_name_query).fetchall()
+    #created_or_sent_successfully, pdf_file_content = create_diploma_and_sign_it(current_student_id)
+    #print(created_or_sent_successfully)
+
+    #signed_file_name_query = "SELECT Signed_file_name from certificates WHERE Student_ID = " + str(current_student_id) + ";"
+    #signed_file_name_query_result = cursor.execute(signed_file_name_query).fetchall()
 
     #file_path_to_check = university_directory_path + str(signed_file_name_query_result[0][0])
-    file_path_to_check = "C:\\Users\\maork\\OneDrive\\Desktop\\Maor Krasner 213225576 diploma.pdf"
+    #file_path_to_check = "/Users/maorkrasner/Desktop/Tom Yanover 213225577.pdf"
 
-    with open(file_path_to_check, "rb") as f_to_check:
-        message_to_check = f_to_check.read()
-        print(str(message_to_check))
+    #with open(file_path_to_check, "rb") as f_to_check:
+    #    message_to_check = f_to_check.read()
+    #    print(str(message_to_check))
 
-    print(verify_if_the_certificate_is_real(current_student_id, message_to_check))
+    #print(verify_if_the_certificate_is_real(current_student_id, message_to_check))
 
     #cursor.execute("DELETE FROM certificates")
     #conn.commit()
 
      #insert_row_into_table("grades", (10, 213225576, 85))
-     #insert_row_into_table("grades", (8, 213225577, 90))
+     #insert_row_into_table("grades", (5, 213225577, 95))
+     #insert_row_into_table("grades", (11, 213225577, 95))
+     #insert_row_into_table("grades", (3, 213225577, 95))
+
      #insert_row_into_table("grades", (11, 213225578, 95))
      #insert_row_into_table("grades", (5, 213225579, 85))
      #insert_row_into_table("grades", (4, 213225580, 97))
@@ -154,4 +182,5 @@ if __name__ == '__main__':
     # insert_row_into_table("subjects", (9, "Object Oriented Programming"))
     # insert_row_into_table("subjects", (10, "Programming in python"))
     # insert_row_into_table("subjects", (11, "Databases and SQL"))
-    # conn.commit()
+
+    #conn.commit()
